f7837acb269b38613b6a0bf3eafd1bc1e0702c16
ishdes <31709580+ishdes@users.noreply.github.com>
2020-09-09 18:07:17 -0500

    Added button support

--- App.js
+++ App.js
@@ -1,89 +1,23 @@
-import * as React from 'react';
-import {
-  Text,
-  View,
-  StyleSheet,
-  Button
-} from 'react-native';
-import * as Permissions from 'expo-permissions';
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import CameraPage from './src/camera.page';
+import FlatButton from './src/button';
 
-import {
-  BarCodeScanner
-} from 'expo-barcode-scanner';
-
-export default class BarcodeScannerExample extends React.Component {
-  state = {
-    hasCameraPermission: null,
-    scanned: false,
-  };
-
-  async componentDidMount() {
-    this.getPermissionsAsync();
-  }
-
-  getPermissionsAsync = async() => {
-    const {
-      status
-    } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({
-      hasCameraPermission: status === 'granted'
-    });
-  };
-
+export default class App extends React.Component {
   render() {
-    const {
-      hasCameraPermission,
-      scanned
-    } = this.state;
-
-    if (hasCameraPermission === null) {
-      return <Text> Requesting
-      for camera permission </Text>;
-    }
-    if (hasCameraPermission === false) {
-      return <Text > No access to camera </Text>;
-    }
-    return ( <
-      View style = {
-        {
-          flex: 1,
-          flexDirection: 'column',
-          justifyContent: 'flex-end',
-        }
-      } >
-      <
-      BarCodeScanner onBarCodeScanned = {
-        scanned ? undefined : this.handleBarCodeScanned
-      }
-      style = {
-        StyleSheet.absoluteFillObject
-      }
-      />
-
-      {
-        scanned && ( <
-          Button title = {
-            'Tap to Scan Again'
-          }
-          onPress = {
-            () => this.setState({
-              scanned: false
-            })
-          }
-          />
-        )
-      } 
+    return (
+      <View style={styles.container}>
+        <FlatButton text='scan qr code' onPress={CameraPage.render}/>
       </View>
     );
   }
-
-  handleBarCodeScanned = ({
-    type,
-    data
-  }) => {
-    this.setState({
-      scanned: true
-    });
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-  };
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
